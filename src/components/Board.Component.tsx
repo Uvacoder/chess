981305bb067781401da9745ecbdb5d222e9ad212
suparthghimire <suparthnarayanghimire2014@gmail.com>
2022-10-09 02:20:47 +0545

    Remove Board Markers

--- src/components/Board.Component.tsx
+++ src/components/Board.Component.tsx
@@ -74,9 +74,9 @@ export default function BoardComponent({
                 else if (board.sound.move) sounds.move.play();
               }}
             >
-              <div className="absolute">
+              {/* <div className="absolute">
                 {x}, {y}
-              </div>
+              </div> */}
               <div className="w-[100%] h-[100%] overflow-hidden">
                 <Piece sprite={cell.piece?.sprite} />
               </div>
