822f8ebf5efa79f8c9190dd17b91f1caa152e3cf
suparthghimire <suparthnarayanghimire2014@gmail.com>
2022-10-15 00:19:44 +0545

    UI Change Check Marker behind Pieces

--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -1,7 +1,7 @@
 export default function Piece({ sprite }: { sprite: string | undefined }) {
   return sprite ? (
     <img
-      className="w-[90px] h-[full] absolute top-[50%] left-[50%]"
+      className="z-[99] w-[90px] h-[full] absolute top-[50%] left-[50%]"
       style={{
         transform: "translate(-50%, -50%)",
       }}
