cb4a7dd4b7bf94f50b8918f1c31bba81ff482415
suparthghimire <suparthnarayanghimire2014@gmail.com>
2022-10-21 00:24:57 +0545

    Change Font for Sidebar

--- src/components/Sidebar.Component.tsx
+++ src/components/Sidebar.Component.tsx
@@ -23,7 +23,7 @@ export default function SideBar({
           className="p-2 flex text-[1.5rem] items-center gap-3 bg-neutral-800 rounded hover:opacity-80"
         >
           <Chess size={20} />
-          <p className="overflow-hidden">Game Info</p>
+          <p className="overflow-hidden text-sans-serif">Game Info</p>
         </button>
         <FenComponent setDrawerOpen={setDrawerOpen} />
         <InfoButtons />
@@ -50,10 +50,10 @@ function FenComponent({ setDrawerOpen }: { setDrawerOpen: Function }) {
 
   return (
     <div className="mt-5">
-      <h2 className="font-bold mb-3 text-lg">
+      <h2 className="font-bold mb-3 text-lg text-sans-serif">
         Set Board Position{" "}
         <a
-          className="italic text-sm underline"
+          className="italic text-sm underline text-sans-serif"
           href="https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation"
           target="_blank"
         >
@@ -61,10 +61,12 @@ function FenComponent({ setDrawerOpen }: { setDrawerOpen: Function }) {
         </a>
       </h2>
       <div>
-        <label htmlFor="fen-text">Enter Fen String</label>
+        <label className="text-lg text-sans-serif" htmlFor="fen-text">
+          Enter Fen String
+        </label>
         <textarea
           rows={6}
-          className={`border-2 resize-none appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+          className={`text-sans-serif border-2 resize-none appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
             fenError && "animate-[shake_0.5s_ease-in-out_1]"
           } ${
             fenError
@@ -82,7 +84,9 @@ function FenComponent({ setDrawerOpen }: { setDrawerOpen: Function }) {
       </div>
       <p className="text-center my-3">-OR-</p>
       <div>
-        <label htmlFor="fen-select">Select From Available Positions</label>
+        <label className="text-lg text-sans-serif" htmlFor="fen-select">
+          Select From Available Positions
+        </label>
         <select
           name="fen-select"
           id="fen-select"
@@ -90,11 +94,15 @@ function FenComponent({ setDrawerOpen }: { setDrawerOpen: Function }) {
           onChange={(e) => {
             setFenString(e.target.value);
           }}
-          className="bg-neutral-800 border-neutral-700 text-white text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none cursor-pointer"
+          className="text-sans-serif  bg-neutral-800 border-neutral-700 text-white text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none cursor-pointer"
         >
           {AVAILABLE_FENS_LABLED.map((fen, idx) => {
             return (
-              <option value={fen.fen} key={`fen-${idx}`}>
+              <option
+                className="text-sans-serif"
+                value={fen.fen}
+                key={`fen-${idx}`}
+              >
                 {fen.name}
               </option>
             );
@@ -107,13 +115,13 @@ function FenComponent({ setDrawerOpen }: { setDrawerOpen: Function }) {
             ChangeFenString(fenString);
             fenError && setDrawerOpen(false);
           }}
-          className="mt-3 w-full bg-neutral-800 hover:bg-neutral-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="text-sans-serif mt-3 w-full bg-neutral-800 hover:bg-neutral-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="button"
         >
           Apply Fen
         </button>
         <button
-          className="mt-3 w-full bg-neutral-800 hover:bg-neutral-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="text-sans-serif mt-3 w-full bg-neutral-800 hover:bg-neutral-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="button"
           onClick={() => {
             setFenString(START_POSITION);
@@ -132,28 +140,28 @@ function GameInfo({ setDrawerOpen }: { setDrawerOpen: Function }) {
   const { fen, pgn, board } = useGame();
 
   return (
-    <div className="mb-5 p-3 grid gap-2">
-      <h2 className="text-[1.5rem] font-bold">Game Info</h2>
+    <div className="mb-5 grid gap-2">
+      <h2 className="text-[1.5rem] font-bold text-sans-serif">Game Info</h2>
       <div className="grid-item">
         <div className="grid-item">
           {board?.game.gameOverInfo.status === false ? (
             <>
               <h2 className="text-lg font-bold">Turn</h2>
-              <p className="text-xl">
+              <p className="text-xl text-sans-serif">
                 It{"'"}s {board?.turn[0].toUpperCase() + board.turn.slice(1)}{" "}
                 {"'"}s Turn
               </p>
             </>
           ) : (
-            <p className="text-xl"> Game Over!</p>
+            <p className="text-xl text-sans-serif">Game Over!</p>
           )}
         </div>
       </div>
       <div className="grid-item">
         <div className="mt-2 flex items-center gap-3">
-          <h2 className="text-lg font-bold">Board FEN</h2>
+          <h2 className="text-lg font-bold text-sans-serif">Board FEN</h2>
           <button
-            className="p-1 bg-neutral-600 rounded-full hover:opacity-80"
+            className="text-sans-serif p-1 bg-neutral-600 rounded-full hover:opacity-80"
             title="Copy Fen"
             onClick={() => {
               CopyTextToClipBoard(fen);
@@ -164,7 +172,7 @@ function GameInfo({ setDrawerOpen }: { setDrawerOpen: Function }) {
             <Copy size={20} />
           </button>
         </div>
-        <div className="mt-1 break-words bg-neutral-800 resize-none appearance-none rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline h-max">
+        <div className="text-sans-serif mt-1 break-words bg-neutral-800 resize-none appearance-none rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline h-max">
           {fen}
         </div>
       </div>
